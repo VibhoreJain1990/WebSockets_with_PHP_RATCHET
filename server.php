72c82eb8ba76d887474fb597575620c1f20e3252
VibhoreJain1990 <57622257+VibhoreJain1990@users.noreply.github.com>
2024-09-24 18:59:25 +0530

    Add files via upload

---
+++ server.php
@@ -0,0 +1,41 @@
+<?php
+require 'vendor/autoload.php';
+
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class MyWebSocket implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(new HttpServer(new WsServer(new MyWebSocket())), 8080);
+echo "WebSocket server is running at ws://localhost:8080\n";
+$server->run();
+echo "after run";
